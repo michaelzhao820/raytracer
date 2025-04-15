21f8640ec9af8ec35695dbeb1ac884d30654568b
Michael Zhao <michaelzhao820@gmail.com>
2025-04-15 12:41:44 -0500

    Can make scene!

--- main.go
+++ main.go
@@ -2,126 +2,98 @@ package main
 
 import (
 	. "github.com/michaelzhao820/raytracer/raytracer"
+	"log"
+	"math"
 )
 
 func main() {
+	world := NewWorld()
 
-	rayOrigin := NewPoint(0, 0, -5)
-
-	wallZPosition := 10.0
-	wallSize := 7.0
-	half := wallSize / 2
-
-	canvasPixels := 100
-	c := NewCanvas(canvasPixels, canvasPixels)
-
-	pixelSize := wallSize / float64(canvasPixels)
-
-	shape := NewSphere()
-	shape.GetMaterial().SetColor(1, 0.2, 1)
-
-	light := Light{
+	// Light source
+	world.SetLight(&Light{
 		Position:  NewPoint(-10, 10, -10),
 		Intensity: NewColor(1, 1, 1),
+	})
+
+	// Floor
+	floor := NewSphere()
+	tfm, _ := ScalingMatrix(10, 0.01, 10)
+	floor.SetTransform(tfm)
+	floor.GetMaterial().SetColor(1, 0.9, 0.9)
+	floor.GetMaterial().SetSpecular(0)
+
+	// Left Wall
+	leftWall := NewSphere()
+	leftTfm, _ := TranslationMatrix(0, 0, 5)
+	ryLeft, _ := RotationYMatrix(-math.Pi / 4)
+	rx, _ := RotationXMatrix(math.Pi / 2)
+	s, _ := ScalingMatrix(10, 0.01, 10)
+
+	lWallTfm, _ := leftTfm.MultiplyMatrices(ryLeft)
+	lWallTfm, _ = lWallTfm.MultiplyMatrices(rx)
+	lWallTfm, _ = lWallTfm.MultiplyMatrices(s)
+
+	leftWall.SetTransform(lWallTfm)
+	leftWall.SetMaterial(floor.GetMaterial())
+
+	// Right Wall
+	rightWall := NewSphere()
+	rightTfm, _ := TranslationMatrix(0, 0, 5)
+	ryRight, _ := RotationYMatrix(math.Pi / 4)
+
+	rWallTfm, _ := rightTfm.MultiplyMatrices(ryRight)
+	rWallTfm, _ = rWallTfm.MultiplyMatrices(rx)
+	rWallTfm, _ = rWallTfm.MultiplyMatrices(s)
+
+	rightWall.SetTransform(rWallTfm)
+	rightWall.SetMaterial(floor.GetMaterial())
+
+	// Middle Sphere
+	middle := NewSphere()
+	mtm, _ := TranslationMatrix(-0.5, 1, 0.5)
+	middle.SetTransform(mtm)
+	middle.GetMaterial().SetColor(0.1, 1, 0.5)
+	middle.GetMaterial().SetDiffuse(0.7)
+	middle.GetMaterial().SetSpecular(0.3)
+
+	// Right Sphere
+	right := NewSphere()
+	scl, _ := ScalingMatrix(0.5, 0.5, 0.5)
+	trn, _ := TranslationMatrix(1.5, 0.5, -0.5)
+	rTx, _ := trn.MultiplyMatrices(scl)
+	right.SetTransform(rTx)
+	right.GetMaterial().SetColor(0.5, 1, 0.1)
+	right.GetMaterial().SetDiffuse(0.7)
+	right.GetMaterial().SetSpecular(0.3)
+
+	// Left Sphere
+	left := NewSphere()
+	scl, _ = ScalingMatrix(0.33, 0.33, 0.33)
+	trn, _ = TranslationMatrix(-1.5, 0.33, -0.75)
+	lTx, _ := trn.MultiplyMatrices(scl)
+	left.SetTransform(lTx)
+	left.GetMaterial().SetColor(1, 0.8, 0.1)
+	left.GetMaterial().SetDiffuse(0.7)
+	left.GetMaterial().SetSpecular(0.3)
+
+	// Add objects to world
+	world.AddObject(floor)
+	world.AddObject(leftWall)
+	world.AddObject(rightWall)
+	world.AddObject(middle)
+	world.AddObject(right)
+	world.AddObject(left)
+
+	// Camera
+	camera := NewCamera(800, 400, math.Pi/3)
+	camera.SetTransform(ViewTransform(
+		NewPoint(0, 1.5, -5),
+		NewPoint(0, 1, 0),
+		NewVector(0, 1, 0),
+	))
+
+	// Render and save
+	if err := camera.Render(*world); err != nil {
+		log.Fatalf("Render failed: %v", err)
 	}
-
-	for y := 0; y < canvasPixels; y++ {
-		for x := 0; x < canvasPixels; x++ {
-			worldX := -half + pixelSize*float64(x)
-			worldY := half - pixelSize*float64(y)
-			position := NewPoint(
-				worldX,
-				worldY,
-				wallZPosition,
-			)
-			direction, _ := position.Subtract(rayOrigin)
-			direction, _ = direction.Normalize()
-			r := NewRay(rayOrigin, direction)
-			xs := r.Intersect(shape)
-			hit := Hit(xs)
-			if hit != nil {
-				point, _ := r.Position(hit.GetTime())
-				normal := hit.GetObject().NormalAt(point)
-				eye := r.Direction().Multiply(-1)
-
-				color := Lighting(*hit.GetObject().GetMaterial(), light, point, eye, normal)
-				c.WritePixel(x, y, color)
-			}
-		}
-	}
-	err := c.CanvasToPPM("sphere.ppm")
-	if err != nil {
-		return
-	}
-
-	/*
-		canvasPixels := 500
-		c := NewCanvas(canvasPixels, canvasPixels)
-
-		clockRadius := float64(canvasPixels) * 3 / 8
-		centerX := canvasPixels / 2
-		centerY := canvasPixels / 2
-
-		p := NewPoint(0, 0, 1)
-
-		for i := 0; i < TotalHours; i++ {
-			r, _ := RotationYMatrix(math.Pi / 6 * float64(i))
-			n, _ := r.MultiplyWithTuple(p)
-
-			scaledX := n[X] * clockRadius
-			scaledZ := n[Z] * clockRadius
-
-			c.WritePixel(centerX+int(scaledX), centerY+int(scaledZ), NewColor(1, 0, 0))
-		}
-
-		err := c.CanvasToPPM("clock.ppm")
-		if err != nil {
-			return
-		}
-	*/
-
-	/*
-		projectile := struct {
-			position Tuple
-			velocity Tuple
-		}{position: NewPoint(0, 1, 0),
-			velocity: NewVector(1, 1.8, 0),
-		}
-		environment := struct {
-			gravity Tuple
-			wind    Tuple
-		}{
-			gravity: NewVector(0, -0.1, 0),
-			wind:    NewVector(-0.01, 0, 0),
-		}
-
-		projectile.velocity, _ = projectile.velocity.Normalize()
-		projectile.velocity = projectile.velocity.Multiply(11.25)
-		c := NewCanvas(900, 550)
-		for projectile.position[Y] > 0 {
-			tick(&projectile, &environment, &c)
-		}
-		err := c.CanvasToPPM("projectile.ppm")
-		if err != nil {
-			return
-		}
-
-	*/
-
-}
-
-func tick(proj *struct {
-	position Tuple
-	velocity Tuple
-}, env *struct {
-	gravity Tuple
-	wind    Tuple
-}, c *Canvas,
-) {
-	proj.position, _ = proj.position.Add(proj.velocity)
-	proj.velocity, _ = proj.velocity.Add(env.gravity)
-	proj.velocity, _ = proj.velocity.Add(env.wind)
-
-	c.WritePixel(int(proj.position[X]), c.Height-int(proj.position[Y])-1, NewColor(1, 0, 0))
-
 }
